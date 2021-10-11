698f47f907c128dc67b309ccd9d74ddf8ae488bf
leandro <leandrochagask@gmail.com>
2021-10-11 18:53:53 -0300

    Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,30 +1,13 @@
 const data = require('../data/zoo_data');
 
-// const actual = getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
-  
-// const species = [{
-//   id: '0938aa23-f153-4937-9f88-4858b24d6bce',
-//   name: 'lions',
-//   popularity: 4,
-//   location: 'NE',
-//   availability: ['Tuesday', 'Thursday', 'Saturday', 'Sunday'],
-//   residents: [
-//     { name: 'Zena', sex: 'female', age: 12 },
-//     { name: 'Maxwell', sex: 'male', age: 15 },
-//     { name: 'Faustino', sex: 'male', age: 7 },
-//     { name: 'Dee', sex: 'female', age: 14 },
-//   ],
-// }];
-
+const species = data.species;
 
 function getSpeciesByIds(...ids) {
-  // seu código aqui
-  const findSpecies = species.filter((specie) => specie.id = ids)
+  result = [];
+  ids.forEach(id => result.push
+    (species.find((specie) => specie.id === id)));
 
-  return findSpecies;
+  return result;
 }
-
-
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
