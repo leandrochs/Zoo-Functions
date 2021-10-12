03be0ce73f2a201886220bb890bf53345d2a670e
leandro <leandrochagask@gmail.com>
2021-10-12 17:15:39 -0300

    Requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
+const {species} = data;
 
-function countAnimals(animal) {
-  // seu código aqui
+const undefinedParam = () => {
+  const obj = {}
+  species.map(({name, residents}) => obj[name] = residents.length);
+
+  return obj;
 }
+
+const specieAndSex = ({specie, sex}) => {
+  let sexFilter = [];
+
+  species.find(({name, residents}) => 
+    (name  === specie) ? sexFilter = residents
+      .filter((resident) => resident.sex === sex) : 0);
+
+  return sexFilter.length;
+}
+
+const justSpecie = ({specie}) => {
+  let n = 0;
+  species.find(({name, residents}) => 
+    (name  === specie) ? n = residents.length : 0);
+
+  return n;
+}
+
+const definedParam = (param) => 
+  (Object.entries(param).length === 2) ? specieAndSex(param) : justSpecie(param);
+
+const countAnimals = (animal) => 
+  (animal === undefined) ? undefinedParam() : definedParam(animal);
 
 module.exports = countAnimals;
