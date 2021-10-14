c438b868ee226cb3cbf0748f98ab1f988ebd45c7
leandro <leandrochagask@gmail.com>
2021-10-13 23:05:12 -0300

    Requisito 9 - getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species, employees } = data;
 
+function getOldestFromFirstSpecies(idParam) {
+  let firstId = '';
+  employees.forEach(({ id, responsibleFor }) => {
+    if (id === idParam) {
+      firstId = responsibleFor[0];
+    }
+  });
 
-  
+  const listAnimals = species.find((specie) => specie.id === firstId).residents
+    .sort((a, b) => b.age - a.age)[0];
+
+  const selectedAnimal = Object.values(listAnimals);
+
+  return selectedAnimal;
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad');
